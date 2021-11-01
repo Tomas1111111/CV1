9fb70887cbacbb1d6333c9a023c7e0ae9830e512
101686 <73076483+101686@users.noreply.github.com>
2021-11-01 15:14:28 +0100

    úprava dílů

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ import {
   DirectionalLight,
   Vector3,
   Color3,
-  SceneLoader
+  SceneLoader,
+  DeviceOrientationCamera
 } from "@babylonjs/core";
 import "@babylonjs/inspector";
 
@@ -21,34 +22,54 @@ canvas.height = window.innerHeight;
 
 const engine = new Engine(canvas, true);
 
-//scéna
+//scéna neměnit
 const scene = new Scene(engine);
 // Default Environment
 
 //vytoření kamery v pozici -5 (dozadu)
-const camera = new UniversalCamera("Camera", new Vector3(0, 5, 10), scene);
+//const camera = new UniversalCamera("Camera", new Vector3(0, 5, 10), scene);
+//const camera = new UniversalCamera("kamera",new Vector3(1,1,10),scene);
+const camera = new DeviceOrientationCamera(
+  "kamera",
+  new Vector3(1, 1, 10),
+  scene
+);
 
 //zaměřit kameru do středu
-camera.setTarget(Vector3.Zero());
+camera.setTarget(new Vector3(0, 1, 0));
 
 //spojení kamery a grafikcého okna
 camera.attachControl(canvas, true);
 
+// var i = 0;
+// for (i = 0; i < 5; i++) {
+//   // Our built-in 'sphere' shape.
+//   var sphere = MeshBuilder.CreateCylinder(
+//     "sphere",
+//     { diameter: i * 0.2, height: 3, segments: 32 },
+//     scene
+//   );
+//   sphere.position.y = 2;
+//   sphere.position.x = i - 2;
+
+//   if (i === 2) {
+//     var blueMat = new StandardMaterial("blueMat", scene);
+//     blueMat.diffuseColor = new Color3(0.5, 0.5, 0.6);
+//     sphere.material = blueMat;
+//   }
+// }
 var i = 0;
 for (i = 0; i < 5; i++) {
-  // Our built-in 'sphere' shape.
   var sphere = MeshBuilder.CreateCylinder(
-    "sphere",
-    { diameter: i * 0.2, height: 3, segments: 32 },
+    "freza",
+    { diameter: 0.2, height: 3 },
     scene
   );
-  sphere.position.y = 2;
-  sphere.position.x = i - 2;
-
+  sphere.position.x = i;
   if (i === 2) {
-    var blueMat = new StandardMaterial("blueMat", scene);
-    blueMat.diffuseColor = new Color3(0.5, 0.5, 0.6);
-    sphere.material = blueMat;
+    var Mat = new StandardMaterial("sedy", scene);
+    Mat.diffuseColor = new Color3(0.5, 0.5, 0.6);
+    sphere.material = Mat;
   }
 }
 
