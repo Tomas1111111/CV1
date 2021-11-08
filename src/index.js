b90d5ee95f07ef153372097370085147231ef57f
Tomas1111111 <93920871+Tomas1111111@users.noreply.github.com>
2021-11-08 14:01:42 +0100

    Nová fréza

--- src/index.js
+++ src/index.js
@@ -85,6 +85,15 @@ SceneLoader.ImportMesh("", "public/", "freza.glb", scene, function (newMeshes) {
   newMeshes[0].rotate(new Vector3(-1, 0, 0), Math.PI / 2);
   newMeshes[0].position.z = -2;
 });
+SceneLoader.ImportMesh("", "public/", "endmill.glb", scene, function (
+  newMeshes
+) {
+  // Set the target of the camera to the first imported mesh
+  newMeshes[0].scaling = new Vector3(0.18, 0.18, 0.18);
+  newMeshes[0].rotate(new Vector3(-1, 0, 0), Math.PI / 2);
+  newMeshes[0].position.z = -2;
+  newMeshes[0].position.x = -2;
+});
 
 //před vykreslením se vždy provede
 scene.registerBeforeRender(function () {
